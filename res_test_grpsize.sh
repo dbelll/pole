9155f8af24f38014906a18acf0e80e9e6773dacb
dbelll <dbelll@yahoo.com>
2010-10-15 14:08:00 -0400

    res_test_grpsize

--- res_test_grpsize.sh
+++ res_test_grpsize.sh
@@ -26,13 +26,25 @@ _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms"
 $_location/pole $_common --AGENT_GROUP_SIZE=1 --TIME_STEPS=524288 --TEST_INTERVAL=8192
 $_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=2048
 $_location/pole $_common --AGENT_GROUP_SIZE=8 --TIME_STEPS=65536 --TEST_INTERVAL=1024
-$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
-$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=1024
-$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=512
+$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=256
+$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=256
 
 
 _sharing="--SHARING_INTERVAL=512"
 _restart="--RESTART_INTERVAL=512"
+_common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms"
+
+$_location/pole $_common --AGENT_GROUP_SIZE=1 --TIME_STEPS=524288 --TEST_INTERVAL=8192
+$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=2048
+$_location/pole $_common --AGENT_GROUP_SIZE=8 --TIME_STEPS=65536 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=512
+$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=512
+$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=512
+
+
+_sharing="--SHARING_INTERVAL=1024"
+_restart="--RESTART_INTERVAL=1024"
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms"
 
 $_location/pole $_common --AGENT_GROUP_SIZE=1 --TIME_STEPS=524288 --TEST_INTERVAL=8192
@@ -43,47 +55,35 @@ $_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVA
 $_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=1024
 
 
-_sharing="--SHARING_INTERVAL=1024"
-_restart="--RESTART_INTERVAL=1024"
-_common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms"
-
-$_location/pole $_common --AGENT_GROUP_SIZE=1 --TIME_STEPS=524288 --TEST_INTERVAL=8192
-$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=8 --TIME_STEPS=65536 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=1024
-
-
 _sharing="--SHARING_INTERVAL=2048"
 _restart="--RESTART_INTERVAL=1024"
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms"
 
-#$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=2048
-#$_location/pole $_common --AGENT_GROUP_SIZE=8 --TIME_STEPS=65536 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=2048
+$_location/pole $_common --AGENT_GROUP_SIZE=8 --TIME_STEPS=65536 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=1024
 
 
 _sharing="--SHARING_INTERVAL=4096"
 _restart="--RESTART_INTERVAL=1024"
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms"
 
-#$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=2048
-#$_location/pole $_common --AGENT_GROUP_SIZE=8 --TIME_STEPS=65536 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=2048
+$_location/pole $_common --AGENT_GROUP_SIZE=8 --TIME_STEPS=65536 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=1024
 
 
 _sharing="--SHARING_INTERVAL=8192"
 _restart="--RESTART_INTERVAL=1024"
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms"
 
-#$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=2048
-#$_location/pole $_common --AGENT_GROUP_SIZE=8 --TIME_STEPS=65536 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=1024
-#$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=2048
+$_location/pole $_common --AGENT_GROUP_SIZE=8 --TIME_STEPS=65536 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=32 --TIME_STEPS=16384 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=1024
 
