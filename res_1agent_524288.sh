8ddc19d4685865d159a1ef4a6ac06d5fb285d849
Dwight Bell <dbelll@yahoo.com>
2010-10-22 15:09:39 -0400

    consolidated sharing code in this branch

--- res_1agent_524288.sh
+++ res_1agent_524288.sh
@@ -9,8 +9,8 @@ _trials="--TRIALS=1024"
 _run="--RUN_ON_CPU=0 --RUN_ON_GPU=1"
 _location="./bin/linux/release"
 
-_restart="--RESTART_INTERVAL=8192"
-_test="--TEST_REPS=8192 --TEST_INTERVAL=8192"
+_restart="--RESTART_INTERVAL=2048"
+_test="--TEST_REPS=8192 --TEST_INTERVAL=16384"
 _sharing="--SHARING_INTERVAL=16384"  # use large value to maximize chunk size
 _wgtinfo="--INIT_SHARING_WGT=0.50 --INIT_THETA_MIN=0.0 --INIT_THETA_MAX=1.0"
 
@@ -18,18 +18,15 @@ _time="--TIME_STEPS=524288"
 
 _grpsize="--AGENT_GROUP_SIZE=1"
 
-_a=.01
+_a=.02
 _e=.00
 _l=.70
 
 
-#for _a in 0.005 0.01 .05
-#do
 
 _parms="--EPSILON=$_e --LAMBDA=$_l --ALPHA=$_a"
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
 $_location/pole $_common $_grpsize $_time $_test $_restart
 
-#done
 
--- res_baseline_timing.sh
+++ res_baseline_timing.sh
@@ -11,7 +11,7 @@ _location="./bin/linux/release"
 
 _test="--TEST_REPS=8192"
 
-_restart="--RESTART_INTERVAL=2048"
+_restart="--RESTART_INTERVAL=8192"
 _sharing="--SHARING_INTERVAL=2048"
 
 _wgtinfo="--INIT_SHARING_WGT=0.50 --INIT_THETA_MIN=-0.1 --INIT_THETA_MAX=0.1"
@@ -24,7 +24,7 @@ _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
-$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=4096
+$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=131072
 
 
 _a=0.10
@@ -35,7 +35,7 @@ _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
-$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=32768
 
 
 _a=0.10
@@ -46,7 +46,7 @@ _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
-$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=256
+$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=8192
 
 
 _a=0.90
@@ -56,5 +56,5 @@ _l=0.70
 _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
-$_location/pole $_common --AGENT_GROUP_SIZE=256 --TIME_STEPS=2048 --TEST_INTERVAL=64
+$_location/pole $_common --AGENT_GROUP_SIZE=256 --TIME_STEPS=2048 --TEST_INTERVAL=2048
 
