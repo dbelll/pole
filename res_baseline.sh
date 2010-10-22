b3c16c9da889cdcbdd1786e3702a6b86b6042769
dbelll <dbelll@yahoo.com>
2010-10-22 15:22:24 -0400

    res_baseline.sh

--- res_baseline.sh
+++ res_baseline.sh
@@ -14,19 +14,14 @@ _test="--TEST_REPS=8192"
 _restart="--RESTART_INTERVAL=2048"
 _sharing="--SHARING_INTERVAL=2048"
 
-<<<<<<< HEAD:res_baseline.sh
-
-_a=0.90
-=======
 _wgtinfo="--INIT_SHARING_WGT=0.50 --INIT_THETA_MIN=-0.1 --INIT_THETA_MAX=0.1"
 
 _a=0.05
->>>>>>> 8ddc19d4685865d159a1ef4a6ac06d5fb285d849:res_baseline.sh
 _e=0.00
 _g=0.90
 _l=0.80
-_parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 
+_parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
 $_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=4096
