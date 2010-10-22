6aaf57b4760ced29b12ff06b6e789c379fe72b7d
dbelll <dbelll@yahoo.com>
2010-10-22 16:08:04 -0400

    testing scripts

---
+++ res_1agent_2097152.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Testing script for res computer
+#
+#  Test restart interavl
+#
+
+_trials="--TRIALS=1024"
+
+_run="--RUN_ON_CPU=0 --RUN_ON_GPU=1"
+_location="./bin/linux/release"
+
+_restart="--RESTART_INTERVAL=2048"
+_test="--TEST_REPS=8192 --TEST_INTERVAL=65536"
+_sharing="--SHARING_INTERVAL=16384"  # use large value to maximize chunk size
+_wgtinfo="--INIT_SHARING_WGT=0.50 --INIT_THETA_MIN=0.0 --INIT_THETA_MAX=1.0"
+
+_time="--TIME_STEPS=2097152"
+
+_grpsize="--AGENT_GROUP_SIZE=1"
+
+_a=.02
+_e=.00
+_l=.70
+
+
+
+_parms="--EPSILON=$_e --LAMBDA=$_l --ALPHA=$_a"
+_common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
+
+$_location/pole $_common $_grpsize $_time $_test $_restart
+
+
--- res_longdur.sh
+++ res_longdur.sh
@@ -17,44 +17,44 @@ _sharing="--SHARING_INTERVAL=2048"
 _wgtinfo="--INIT_SHARING_WGT=0.50 --INIT_THETA_MIN=-0.1 --INIT_THETA_MAX=0.1"
 
 _a=0.05
-_e=0.02
+_e=0.00
 _g=0.90
 _l=0.80
 
 _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
-$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=131072 --TEST_INTERVAL=4096
+$_location/pole $_common --AGENT_GROUP_SIZE=4 --TIME_STEPS=524288 --TEST_INTERVAL=16384
 
 
 _a=0.10
-_e=0.02
+_e=0.00
 _g=0.90
 _l=0.90
 _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
-$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=32768 --TEST_INTERVAL=1024
+$_location/pole $_common --AGENT_GROUP_SIZE=16 --TIME_STEPS=131072 --TEST_INTERVAL=4096
 
 
 _a=0.10
-_e=0.02
+_e=0.00
 _g=0.90
 _l=0.90
 _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
-$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=8192 --TEST_INTERVAL=256
+$_location/pole $_common --AGENT_GROUP_SIZE=64 --TIME_STEPS=32768 --TEST_INTERVAL=1024
 
 
 _a=0.90
-_e=0.02
+_e=0.00
 _g=0.90
 _l=0.70
 _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
-$_location/pole $_common --AGENT_GROUP_SIZE=256 --TIME_STEPS=2048 --TEST_INTERVAL=64
+$_location/pole $_common --AGENT_GROUP_SIZE=256 --TIME_STEPS=8192 --TEST_INTERVAL=256
 
