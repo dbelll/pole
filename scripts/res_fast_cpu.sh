640e011f0ba99d469f4597733c0a4e5474b0e732
dbelll <dbelll@yahoo.com>
2010-10-25 11:45:31 -0400

    makefile and scripts

--- scripts/res_fast_cpu.sh
+++ scripts/res_fast_cpu.sh
@@ -9,7 +9,7 @@ _location="./bin/linux/release"
 _test="--TEST_REPS=8192"
 #_restart="--RESTART_INTERVAL=2048"
 _sharing="--SHARING_INTERVAL=16"
-_wgtinfo="--INIT_SHARING_WGT=0.50 --INIT_THETA_MIN=-0.1 --INIT_THETA_MAX=0.1"
+_wgtinfo="--INIT_SHARING_WGT=0.50 --INIT_THETA_MIN=-1.0 --INIT_THETA_MAX=0.1"
 
 
 #--------------
@@ -28,9 +28,9 @@ _run="--RUN_ON_CPU=1 --RUN_ON_GPU=0"
 _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
-$_location/pole $_common --AGENT_GROUP_SIZE=1 --TIME_STEPS=524288 --TEST_INTERVAL=524288
+#$_location/pole $_common --AGENT_GROUP_SIZE=1 --TIME_STEPS=524288 --TEST_INTERVAL=524288
 
-_trials="--TRIALS=16"
+#_trials="--TRIALS=16"
 
 for _a in .05
 do
@@ -40,6 +40,6 @@ _run="--RUN_ON_CPU=1 --RUN_ON_GPU=1"
 _parms="--ALPHA=$_a --EPSILON=$_e --GAMMA=$_g --LAMBDA=$_l"
 _common="$_trials $_run $_grpsize $_restart $_test $_sharing $_parms $_wgtinfo"
 
-$_location/pole $_common --AGENT_GROUP_SIZE=1 --TIME_STEPS=256 --TEST_INTERVAL=16
+$_location/pole $_common --AGENT_GROUP_SIZE=1 --TIME_STEPS=1 --TEST_INTERVAL=1
 
 done
